741372b19c77ed0eea6225e61359bdbc9421211b
André Schneider <24269043+WDD-AS@users.noreply.github.com>
2022-04-13 11:07:52 +0200

    Add files via upload

---
+++ elementor-classfix.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name:       Elementor Deprecated Class Fix
+ * Description:       Fix Elementor Class Fatal Error (Scheme_Color, Scheme_Typography)
+ * Version:           1.0.0
+ * Author:            WebDevDesign by André Schneider
+ * Author URI:        https://webdevdesign.de/
+ * License:           MIT
+ **/
+
+namespace Elementor;
+
+add_action( 'plugins_loaded', function() {
+    if ( ! class_exists( 'Elementor\Scheme_Color' ) ) {
+      class Scheme_Color extends Core\Schemes\Color {}
+    }
+	
+	if ( ! class_exists( 'Elementor\Scheme_Typography' ) ) {
+      class Scheme_Typography extends Core\Schemes\Typography {}
+    }
+  }
+);
